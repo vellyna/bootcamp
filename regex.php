821c29bed0f91a181cffbbf5d8fa3bb177f54453
Vellyna <34593800+vellyna@users.noreply.github.com>
2019-08-31 18:02:17 +0700

    Add regex files

---
+++ regex.php
@@ -0,0 +1,28 @@
+<?php
+$username1 = 'Vellyna15A';
+$username2 = '15Vellyna15';
+$password1 = 'passW0rd=';
+$password2 = 'C0d3YourFuture!#'; 
+function is_username_valid($username)
+{
+    if(preg_match("/^([a-zA-Z][a-zA-Z0-9]{5,9}$)+/", $username)) 
+    {
+        echo "TRUE<br>";
+    } else {
+        echo "FALSE<br>";
+    }
+}
+function is_password_valid($password)
+{
+    if(preg_match("/^(?=.{8,}$)(?=.[a-z])(?=.*?[A-Z])(?=.*?[0-9])(?=.*?\W).*$/", $password)) 
+    {
+        echo "TRUE<br>";
+    } else {
+        echo "FALSE<br>";
+    }
+}
+is_username_valid($username1);
+is_username_valid($username2);
+is_password_valid($password1);
+is_password_valid($password2);
+?>
