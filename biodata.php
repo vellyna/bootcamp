d3279b8260cc7f06029044e4f59e1abca9bd0b27
Vellyna <34593800+vellyna@users.noreply.github.com>
2019-08-31 16:26:53 +0700

    Add biodata files

---
+++ biodata.php
@@ -0,0 +1,18 @@
+<?php
+header('Content-type: application/json');
+$biodata = array(
+	'name' => ('Vellyna Angelicha Sitorus'), 
+	'age' => 20, 
+	'address' => ('Jalan SM. Raja Km. 11,5 Medan'),
+	'hobbies' => array('Learn to play the guitar','Coding'),
+	'is_maried' => false,
+	'list_school' => array('name' => ['SMKN BI PROVSU','USU'], 
+                        'year_in' => ['2012','2015']), 
+                        'year_out' => ['2015','2019'], 
+                        'major' => ['Computer & Network Engineering','Computer Science'],
+	'skills' => array('skill_name' => ['c/c++','java','c#'], 'level' => ['advanced','advanced','advanced']),
+	'interest_in_coding' => true,
+);
+$json = json_encode($biodata);
+echo $json;
+?>
